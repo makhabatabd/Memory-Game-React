33dfe6bb3b2425fc4adc9aa6491784fcbb24f029
makhabatabd <makhabat9898@gmail.com>
2024-06-28 12:43:58 -0700

    final project memory game for beginners

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import initialCards from "./data/initialCard";
+import Board from "./components/Board";
+import GameInfo from "./components/GameInfo";
 
 function App() {
+  const [cards, setCards] = useState(initialCards);
+  const [flippedCards, setFlippedCards] = useState([]);
+  const [matchedCards, setMatchedCards] = useState([]);
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  useEffect(() => {
+    setCards(shuffleCards(initialCards));
+  }, []);
+
+  useEffect(() => {
+    if (flippedCards.length === 2) {
+      setIsDisabled(true);
+      const [first, second] = flippedCards;
+      if (cards[first].image === cards[second].image) {
+        setMatchedCards([...matchedCards, first, second]);
+        setFlippedCards([]);
+        setIsDisabled(false);
+      } else {
+        setTimeout(() => {
+          const newCards = cards.map((card, index) => {
+            if (index === first || index === second) {
+              card.isFlipped = false;
+            }
+            return card;
+          });
+          setCards(newCards);
+          setFlippedCards([]);
+          setIsDisabled(false);
+        }, 1000);
+      }
+    }
+  }, [cards, flippedCards, matchedCards]);
+
+  const hadleCardClick = (index) => {
+    if (
+      isDisabled ||
+      flippedCards.includes(index) ||
+      matchedCards.includes(index)
+    )
+      return;
+    const newCards = cards.map((card, i) => {
+      if (i === index) {
+        card.isFlipped = true;
+      }
+      return card;
+    });
+    setCards(newCards);
+    setFlippedCards([...flippedCards, index]);
+  };
+
+  const resetGame = () => {
+    const resetCards = initialCards.map((card) => ({
+      ...card,
+      isFlipped: false,
+    }));
+    setCards(shuffleCards(resetCards));
+    setFlippedCards([]);
+    setMatchedCards([]);
+    setIsDisabled(false);
+  };
+
+  const shuffleCards = (cards) => {
+    return cards.sort(() => Math.random() - 0.5);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Memory Game</h1>
+      <Board cards={cards} onCardClick={hadleCardClick} />
+      <GameInfo matchedCards={matchedCards} onReset={resetGame} />
     </div>
   );
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Card({ index, card, onClick }) {
+  return (
+    <div
+      className={`card ${card.isFlipped ? "flipped" : ""}`}
+      onClick={() => onClick(index)}
+    >
+      <div className="card-inner">
+        <div className="card-front">
+          <img src={card.image} alt="card front of an animal" />
+        </div>
+        <div className="card-back"></div>
+      </div>
+    </div>
+  );
+}
